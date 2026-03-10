4787bbe5422483b3a856cdeb66a25d75fe31929d
jakeyvee <jakeyveee@gmail.com>
2026-03-10 17:14:08 +0800

    add claim rewards with index scripts for kvault and market strategies

--- config/kamino.ts
+++ config/kamino.ts
@@ -3,9 +3,11 @@ export const withdrawStrategyAmount = Number.MAX_SAFE_INTEGER;
 
 // FOR KVAULT STRATEGY
 export const kvaultAddress = "";
+export const kvaultRewardIndex = 0;
 
 // FOR MARKET STRATEGY
 export const reserveAddress = "";
+export const marketRewardIndex = 0;
 
 // DIRECT WITHDRAW
 export const directWithdrawDiscriminator: number[] = [];
